79d220ab5e099804186b28515016f62652e3c5dc
Li Yu <gachuka@gmail.com>
2023-02-27 13:43:48 -0500

    Base game complete

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,12 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
 
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
+    public bool gameIsAlive = true;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        if (gameIsAlive)
+        {
+            playerScore = playerScore + scoreToAdd;
+            scoreText.text = playerScore.ToString();
+        }
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+        gameIsAlive = false;
+    }
 
     // Start is called before the first frame update
     void Start()
